07a649495a7e35a24f8936b963b9e2b350c79647
luxo-ai <luxo.ai@proton.me>
2022-10-05 11:35:47 -0400

    disable default mac behavior when holding down a key

--- setup.sh
+++ setup.sh
@@ -2,24 +2,24 @@
 
 brew update
 brew install jq \
-             httpie \
-             nvm \
-             python3
+	httpie \
+	nvm \
+	python3
 
 
 # yarn for javascript/typescript development
 if ! command -v yarn >/dev/null 2>&1;
 then 
-    echo "installing yarn..."
-    # npm available through nvm
-    npm install --global yarn
+	echo "installing yarn..."
+	# npm available through nvm
+	npm install --global yarn
 fi 
 
 # haskell development
 if ! command -v stack >/dev/null 2>&1;
 then
-    echo "installing stack w ghc..."
-    curl -sSL https://get.haskellstack.org/ | sh
+	echo "installing stack w ghc..."
+	curl -sSL https://get.haskellstack.org/ | sh
 fi
 
 echo "Git setup..."
@@ -30,3 +30,5 @@ echo "Zsh setup..."
 ./zsh-setup.sh
 echo "Adding local bin..."
 ./add-local-bin.sh
+echo "Setting key repeat on hold instead of special char..."
+defaults write -g ApplePressAndHoldEnabled -bool false
